538f25ad0b9b3a8fffa91421b3d29a8112edd35b
Yashwin Tapdiya <yashwintapdiya@gmail.com>
2024-02-06 23:31:48 +0530

    setting routing and tailwind

---
+++ src/MessageBoard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MessageBoard = () => {
+  return (
+    <div>
+      <h2>MessageBoard</h2>
+    </div>
+  );
+};
+
+export default MessageBoard;
---
+++ src/PostView.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PostView = () => {
+  return (
+    <div>
+      <h2>PostView</h2>
+    </div>
+  );
+};
+
+export default PostView;
